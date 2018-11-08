75be5914b18b6757b4601d40140c06df47b47013
Vishwesh Bhardwaj <vishweshbhardwaj88@gmail.com>
2018-11-08 14:11:03 +0530

    Initial Commit
    - Project Created.

---
+++ MyParkingAssistant/MyParkingAssistant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyParkingAssistant
+//
+//  Created by Aatreya on 08/11/18.
+//  Copyright © 2018 LogicAnalyzers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
